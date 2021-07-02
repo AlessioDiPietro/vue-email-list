4a2deccf4bf24ab57c6b780c8cf006383b8d8fa2
AlessioDPT <alessio.recos@gmail.com>
2021-07-02 16:38:06 +0200

    Fine- Step1

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@ const app = new Vue({
         emailList: []
     },
     mounted(){
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(result =>{
-                console.log(result.data.response);
-                this.emailList.push(result.data.response)
-            })
+        for (i = 0; i < 10; i++){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(result =>{
+                    this.emailList.push(result.data.response)
+                    console.log(this.emailList)
+                })
+        }
         
     }
 
